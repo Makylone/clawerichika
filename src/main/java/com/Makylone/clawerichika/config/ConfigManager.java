9dd16fc9c32723dd89d7bbe746cd34c11cc5c732
Warheart-1 <axel97477@gmail.com>
2025-12-19 00:23:13 +0100

    fixing config with the env variable

--- src/main/java/com/Makylone/clawerichika/config/ConfigManager.java
+++ src/main/java/com/Makylone/clawerichika/config/ConfigManager.java
@@ -1,24 +1,12 @@
 package com.Makylone.clawerichika.config;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 public class ConfigManager {
 
-    private static final Dotenv dotenv = Dotenv.load();
-
     public static String GetToken() {
-        if(!dotenv.get("TOKEN_DISCORD").isEmpty() && dotenv.get("TOKEN_DISCORD") != null){
-            return dotenv.get("TOKEN_DISCORD");
-        } else {
-            return System.getenv("TOKEN_DISCORD");
-        }  
+        return System.getenv("TOKEN_DISCORD");
     }
 
     public static String GetAdminRoleId() {
-        if(!dotenv.get("ADMIN_ROLE_ID").isEmpty() && dotenv.get("ADMIN_ROLE_ID") != null){
-            return dotenv.get("ADMIN_ROLE_ID");
-        } else {
-            return System.getenv("ADMIN_ROLE_ID");
-        }
+        return System.getenv("ADMIN_ROLE_ID");
     }
 }
